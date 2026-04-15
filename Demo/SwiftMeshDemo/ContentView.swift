a51a18b995e297465d522bcc389db4ff1f936f47
Jonathan Wight <jwight@mac.com>
2026-04-14 17:26:44 -0700

    Add SwiftMeshDemo Xcode project with configuration and support package

---
+++ Demo/SwiftMeshDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftMeshDemo
+//
+//  Created by Jonathan Wight.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Demo/SwiftMeshDemo/SwiftMeshDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMeshDemoApp.swift
+//  SwiftMeshDemo
+//
+//  Created by Jonathan Wight.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMeshDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
